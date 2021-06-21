82bee9630bbeb44fd4f4bbce8504e8908aebc7a4
shashikiran918 <85386298+shashikiran918@users.noreply.github.com>
2021-06-21 20:39:30 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MaterialApp(
+    home: Home()
+));
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("my first App"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("hello world"),
+          FlatButton(
+              onPressed: () {} ,
+              color: Colors.amber,
+              child: Text("click me")
+          ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(30.0),
+            child: Text("inside container"),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("click"),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+
+  }
+}
